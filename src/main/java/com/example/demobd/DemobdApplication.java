a053788739c6cb1f4e55b85f4fb3e588fdd7dd03
Ana Carolina Lobato <carolina.lobato@dtidigital.com.br>
2020-03-26 16:58:56 -0300

    Removing code smells

--- src/main/java/com/example/demobd/DemobdApplication.java
+++ src/main/java/com/example/demobd/DemobdApplication.java
@@ -1,13 +1,7 @@
 package com.example.demobd;
 
-import com.github.mongobee.Mongobee;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.mongodb.core.MongoTemplate;
 
 @SpringBootApplication
 public class DemobdApplication {
--- src/main/java/com/example/demobd/config/MongobeeConfiguration.java
+++ src/main/java/com/example/demobd/config/MongobeeConfiguration.java
@@ -2,21 +2,15 @@ package com.example.demobd.config;
 
 import com.example.demobd.properties.MongoConfiguration;
 import com.github.mongobee.Mongobee;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
 @Configuration
 public class MongobeeConfiguration {
-    @Autowired
-    private MongoTemplate mongoTemplate;
-
-    @Autowired
-    private MongoConfiguration mongoConfiguration;
 
     @Bean
-    public Mongobee mongobee() {
+    public Mongobee mongobee(MongoTemplate mongoTemplate, MongoConfiguration mongoConfiguration) {
         Mongobee mongobee = new Mongobee(mongoConfiguration.getUri());
         mongobee.setMongoTemplate(mongoTemplate);
         mongobee.setChangeLogsScanPackage("com.example.demobd.db.migrations");
